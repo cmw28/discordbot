07760957d70e2f6db32efb55240aa3bee115f847
Christian Waite <cmw28@scasd.org>
2020-11-05 14:30:09 -0500

    please the linter

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const client = new Discord.Client();
 const config = require('./config.json');
 
 client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('guildMemberAdd', member => {
-	console.log(`Welcoming ${member.name} to ${member.guild.name}`);
-	member.guild.systemChannel.send(`Welcome to ${member.guild.name}, ${member.name}!`);
+  console.log(`Welcoming ${member.name} to ${member.guild.name}`);
+  member.guild.systemChannel.send(`Welcome to ${member.guild.name}, ${member.name}!`);
 });
 
 client.login(config.token);
