12c115ec130bf5f468871bc1c17362d94bcb8530
Christian Waite <cmw28@scasd.org>
2020-11-06 17:57:07 -0500

    stop using a json config in favor of .env

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+// require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs'); // we need this to read ./events
 
 try { // try to get the config
-  var config = require('./config.json');
+  require('dotenv').config();
 } catch { // if that goes wrong... complain and exit
-  console.error('There is no config! Please copy ./defaultconfig.json to ./config.json and add a token.');
+  console.error('There is no .env! Please copy example.env to .env and add a token.');
   process.exit();
 }
 
@@ -19,7 +20,7 @@ fs.readdir('./events/', (err, files) => {
 });
 
 // client.login is a promise, so i just have to tack on a .catch for if it goes wrong
-client.login(config.token).catch(() => { // try to log in. if it fails, complain and exit.
-  console.error('Failed to log in! Make sure ./config.json has a valid token.');
+client.login(process.env.TOKEN).catch(() => { // try to log in. if it fails, complain and exit.
+  console.error('Failed to log in! Make sure .env has a valid token.');
   process.exit();
 });
