4209c1adac8db5ccc3497f58b6509f9f601a7787
Christian Waite <cmw28@scasd.org>
2020-11-06 09:48:52 -0500

    comments

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require('fs');
+const fs = require('fs'); // we need this to read ./events
+let config; // creates a config thing for the try catch block to put the config into
 
-let config;
-// check if there actually is a config. if not, complain and exit.
-try {
+try { // try to get the config
   config = require('./config.json');
-} catch {
+} catch { // if that goes wrong... complain and exit
   console.error('There is no config! Please copy ./defaultconfig.json to ./config.json and add a token.');
   process.exit();
 }
@@ -20,8 +19,8 @@ fs.readdir('./events/', (err, files) => {
   });
 });
 
-// try to log in. if it fails, complain and exit.
-client.login(config.token).catch(() => {
+// client.login is a promise, so i just have to tack on a .catch for if it goes wrong
+client.login(config.token).catch(() => { // try to log in. if it fails, complain and exit.
   console.error('Failed to log in! Make sure ./config.json has a valid token.');
   process.exit();
 });
