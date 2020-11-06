582486896be7850fe67e8cf0fb79316fe852eab9
Christian Waite <cmw28@scasd.org>
2020-11-06 09:34:33 -0500

    checking for config, checking for token in config

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
 const fs = require('fs');
+
+let config;
+// check if there actually is a config. if not, complain and exit.
+try{
+  config = require('./config.json');
+} catch {
+  console.error('There is no config! Please copy ./defaultconfig.json to ./config.json and add a token.');
+  process.exit();
+}
 
 fs.readdir('./events/', (err, files) => {
   if (err) return console.log.error(err);
@@ -12,4 +20,8 @@ fs.readdir('./events/', (err, files) => {
   });
 });
 
-client.login(config.token);
+// try to log in. if it fails, complain and exit.
+client.login(config.token).catch(() => {
+  console.error('Failed to log in! Make sure ./config.json has a valid token.');
+  process.exit();
+});
